f5b1b1defc8cfbca08af206f91d56176643b296d
David <dmurenzi@ac.rica.rw>
2023-08-13 08:36:32 -0700

    third upload

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/* progaram that puts a string into stdio */
+#include<stdio.h>
+/**
+*main-call the header to include stdio file
+*str1:decralation of string
+*
+*puts-function-helps to put a character to the console
+*Return:-zero when the program runs successfully
+*/
+
+int main(void)
+{
+	char str1[] = "Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
+}
