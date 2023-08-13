bd1faae2641b1f82e81f82da8dd1abb42cfb4fbf
David <dmurenzi@ac.rica.rw>
2023-08-13 14:05:22 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/* the program to write something without printf and puts functions */
+#include<unistd.h>
+/**
+ * main-an entry function
+ * write-use to print to thestandard error
+ * Return: 1 when the progrma run well
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n, 59);
+
+	return 1;
+}
