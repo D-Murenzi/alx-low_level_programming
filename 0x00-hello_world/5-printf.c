a3c326b929909d7d5ccf96fb674e7cd6d4f38965
David <dmurenzi@ac.rica.rw>
2023-08-13 09:02:11 -0700

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/* this program uses printf function to put string on the console*/
+#include<stdio.h>
+/**
+ * main-is the entry function
+ * printf-will put the string to console
+ * Return: 0 when the program runs success fully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
