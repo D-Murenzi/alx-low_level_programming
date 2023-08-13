42f1d83e8b18be2e313416525711dcb193fa0748
David <dmurenzi@ac.rica.rw>
2023-08-13 13:05:49 -0700

    sixth

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/*this program writes the sizeof various types on computer its compiled on */
+#include<stdio.h>
+/**
+ * main-call the other functions from the lib
+ * printf-allows to print on the default output
+ * Return: 0 when the code is run successfully
+ */
+int main(void)
+{
+	printf("Size of a char: %lu\n", (unsigned long int)sizeof(char));
+	printf("Size of an int: %lu\n", (unsigned long int)sizeof(int));
+	printf("Size of a long int: %lu\n", (unsigned
+					     long int)sizeof(long int));
+	printf("Size of a long long int: %lu\n", (unsigned long int)sizeof(long
+long int));
+	printf("Size of a float: %lu\n", (unsigned long int)sizeof(float));
+
+	return (0);
+}
