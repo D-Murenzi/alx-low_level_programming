2eca2d7409069de7828b9347771818c1107d0455
David <dmurenzi@ac.rica.rw>
2023-08-14 13:45:21 -0700

    this crushed the machine

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/* program that prints alphabets in different notations */
+#include <stdio.h>
+/**
+ * main-entry point of the code
+ * for-loop the printing of the characters
+ * putchar- prints the character to the std output
+ * Return: 0 when successfully run
+ */
+int main(void)
+{
+        int a;
+	int A;
+
+        for (A = 'A';((A >= 65) && (A <= 90)); A++)
+        {
+                putchar(A);
+
+        }
+	for (a = 'a'; ((a >= 97) && (a <=122)); a++)
+	{
+		putchar(a);
+	}
+        putchar('\n');
+
+        return (0);
+}
