433807a1fc11d89c4c719006ea970077d2e06ead
David <dmurenzi@ac.rica.rw>
2023-08-14 12:45:45 -0700

    another commit for the day

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/* program that prints alphabets in different notations */
+#include <stdio.h>
+/**
+ * main-entry point of the code
+ * for-loop the printing of the characters
+ * putchar- prints the character to the std output
+ * Return: 0 when successfully run
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; (a >= 97) && (a <= 122); a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+
+	return (0);
+}
