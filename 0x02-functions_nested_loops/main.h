f1f80a6686c60adf4e73cb0f8808bff65e71b639
David <dmurenzi@ac.rica.rw>
2023-08-16 15:03:24 -0700

    mm

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,10 @@
 /* this is the header file for many projects to call on*/
 #include <stdio.h>
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+// Declare the _putchar function
 int _putchar(void);
+
+#endif
 
