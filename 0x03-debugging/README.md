06107120ba381b8ddc48d9f5ccd68244402d39ed
David <dmurenzi@ac.rica.rw>
2023-08-17 14:18:51 -0700

    first

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+this folder is for project debugging.
+it contains different files each file containing a code for apecific task of
+the project
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+/* this is a header file for the project */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
