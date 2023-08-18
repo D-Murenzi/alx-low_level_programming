f48f0dd061f987d5ef68181889ae8e64261303cd
David <dmurenzi@ac.rica.rw>
2023-08-18 13:15:44 -0700

    function that checks if input is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+/* this functions checks if an input from user is a digit or not */
+#include <stdio>
+
+/**
+ * _isdigit-function takes input and tells if it digit or not
+ * @c: is an interger input
+ * Return: 1 if its digit and 0 if its not
+ */
+
+int _isdigit(int c)
+{
+	int d;
+
+	if ((d >= 48) && (d <= 57))
+	{
+		d = 1;
+	}
+	else
+	{
+		d = 0;
+	}
+	return (d);
+}
