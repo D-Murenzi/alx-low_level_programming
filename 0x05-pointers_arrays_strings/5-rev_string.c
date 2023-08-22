0a15a8e92524f3907e8116a377c1287488c6e39a
David <dmurenzi@ac.rica.rw>
2023-08-22 15:28:32 -0700

    another update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
+
+/**
  * rev_string-reverses the string
  * @s: this is a string input
  * Return: void
