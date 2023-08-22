bb6cca952ceec61b18156c8e520bfa7768c75565
David <dmurenzi@ac.rica.rw>
2023-08-22 08:19:47 -0700

    first commit about pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/* this program is a function that takes a point to an integer*/
+/* as an argument and updates the int to 98*/
+
+#include "main.h"
+
+/**
+ * reset_to_98-function takes an interger and change its value
+ * @n:is an a pointer to the integer to change its value
+ * Return: void on sucess
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This folder contains many files about pointers
+there are different tasks that makes this project and each
+of the task has its own special file.
+it also contains main.h file which is a header file that contains all the
+prototypes of different used functions.
