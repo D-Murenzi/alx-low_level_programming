7d0e67e5afeee5f018fde3b59dc3acc9a56a621a
David <dmurenzi@ac.rica.rw>
2023-08-24 14:39:30 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+/* this program will concatenate two strings till nth element of the second*/
+#include "main.h"
+/**
+ * _strncat-function to concatenate two strings
+ * @dest:a string input
+ * @src: another string input
+ * @n: this is interger to stop concatanation
+ * Return: pointer to new string array
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *first;
+	int a;
+
+	first = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	for (a = 0; a <= n; a++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (first);
+}
