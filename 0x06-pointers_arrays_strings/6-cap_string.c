9cf6219f69d6cabad205392e1d4dbe2f138d5a90
David <dmurenzi@ac.rica.rw>
2023-08-25 15:47:03 -0700

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+/* this function will capitilize all words of the string */
+#include "main.h"
+/**
+ * cap_string-capitilize each word of the sentence
+ * @str: pointer to the sentece or string to be capitilized
+ * Return: pointer to the string
+ */
+
+char *cap_string(char *str)
+{
+	char *original;
+
+	original = str;
+	while (*str != '\0')
+	{
+		if (!(((*str >= 65) && (*str <= 90))
+		      && ((*str >= 97) && (*str <= 122))))
+		{
+			str++;
+			if (!(((*str >= 65) && (*str <= 90))
+			      && ((*str >= 97) && (*str <= 122))))
+			{
+				if ((*str >= 97) && (*str <= 122))
+				{
+					*str = *str - 32;
+				}
+			}
+		}
+		else
+		{
+			str++;
+		}
+	}
+	return (original);
+}
