e42ec722cfc5d5d8e3fbdb450280a0571778e735
David <dmurenzi@ac.rica.rw>
2023-08-30 15:02:30 -0700

    correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,14 @@
 /* this function prints a string to the stdout */
 #include "main.h"
 /**
+ * _putchar-function to putchar to stdoutput
+ * @c:a character to be printed
+ * Return: interger
+ */
+
+int _putchar(char c);
+
+/**
  * _puts_recursion-function to print a string
  * @s: pointer to the string to be printed
  * Return: nothing
@@ -10,6 +18,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return ();
 	}
 	_putchar(*s);
