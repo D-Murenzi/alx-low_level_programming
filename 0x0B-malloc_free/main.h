5c4b4a535074c6f145437f92247b086c656a0cf9
David <dmurenzi@ac.rica.rw>
2023-09-05 08:46:11 -0700

    correction of mai header

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
 /* this is a header file for this project */
-#include <stdio>
+#include <stdio.h>
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
