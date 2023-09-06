a714d548bb4c1af33611a5073200f598b8d6093d
David <dmurenzi@ac.rica.rw>
2023-09-06 13:10:52 -0700

    freeing memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,31 @@
+/* this function free memory that was previously reserved by alloc_grid func*/
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid-function that releases all memory that was previously reserved
+ * @grid:pointer to the reserved memory
+ * @height:being the number of elements in each sub array
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL)
+	{
+		return (void);
+	}
+	else if ((grid <= 0) || (height <= 0))
+	{
+		free(grid);
+		return (void);
+	}
+	else
+	{
+		for (a = 0; a < height; a++)
+		{
+			free(grid[a]);
+		}
+		free(grid);
+		return (void);
+	}
+}
