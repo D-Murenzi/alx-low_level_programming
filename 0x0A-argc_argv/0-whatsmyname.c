7580db460eda6391ca4eddfa5ad3c90a5dfb3c0e
David <dmurenzi@ac.rica.rw>
2023-09-06 16:42:25 -0700

    discovery

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,17 @@
 /* this function prints its name*/
-#nclude "main.h"
+#include "main.h"
 #include <stdlib.h>
 /**
- * _main-call the function and prints its name
+ * main-call the function and prints its name
  * @argc: number arguments main function receives
  * @argv:array of strings made of arguments
  * Return: 0 on success
  */
-int _main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc >= 1)
 	{
-		printf("%s", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
