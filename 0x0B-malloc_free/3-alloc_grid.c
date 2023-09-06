312052b0f704cdcbb8b040ecb2fd9c170924c193
David <dmurenzi@ac.rica.rw>
2023-09-06 10:32:45 -0700

    2-alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+/* this function allocates memory for a 2 dimensional array */
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid-function that allocates amemory to 2-dime array
+ * @width:number of arrays held by the 2-D array
+ * @height:element that makes the subarrays
+ * Return: pointer to the memory if success or NUll in failer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int a;
+	int b;
+
+
+	if ((width <= 0) || (height <= 0))
+	{
+		return (NULL);
+	}
+	grid = malloc(width * sizeof(int *));
+	for (a = 0; a < width; a++)
+	{
+		grid[a] = malloc(height * sizeof(int));
+
+		/*now each element of grid holds a pointer to a memory that*/
+		/*can hold an array of size height*/
+
+		for (b = 0; b < height; b++)
+		{
+			grid[a][b] = 0;
+			/* keeping in mind that grid[a] is apointer */
+			/* grid[a] can have index grid[a][b] which is value*/
+			/* held by grid[a], so the above line assigned it to 0*/
+		}
+	}
+	return (grid);
+}
