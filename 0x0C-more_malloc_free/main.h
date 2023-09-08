90054f09cc7195b1266b2bfcf97729f761cdaa78
David <dmurenzi@ac.rica.rw>
2023-09-08 16:35:54 -0700

    this thing is tough

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,8 @@
+#ifndef MAIN_H
+#def MAIN_H
 #include <stdio.h>
+#include <sdtlib.h>
+#include <string.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
@@ -6,3 +10,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
+#endif
