4896624dc2d4733cb33ed673adfbaab924824494
David <dmurenzi@ac.rica.rw>
2023-09-11 15:39:12 -0700

    macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+/* this program prints the name of the file*/
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main-call other function
+ * Return: 0 on sucess
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
