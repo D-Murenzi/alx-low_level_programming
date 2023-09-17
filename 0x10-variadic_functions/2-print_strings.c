31cc68eee450e5ea06e4549f01a670b449313b91
David <dmurenzi@ac.rica.rw>
2023-09-17 15:35:36 -0700

    print numbers and print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", str);
 		}
-		if (separator != NULL)
+		if ((separator != NULL) && (i < (n - 1)))
 		{
 			printf("%s", separator);
 		}
