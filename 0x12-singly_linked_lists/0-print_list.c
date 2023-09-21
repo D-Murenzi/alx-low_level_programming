f6f8cee714bf10eb60a52f9e8212e822a2f291b9
D-Murenzi <murenzidavid01@gmail.com>
2023-09-21 13:40:22 -0700

    new push, made of header file, o-print_list and readme"

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,44 @@
+/* this function prints all the elements of list_t list which is a linked list*/
+
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list-function to print elements of list
+ * @h: pointer to the linked list
+ * Return: number of nodes of the linked lists
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (h != NULL)
+	{
+		int numbers;
+		const list_t *ptr;
+
+		numbers = 0;
+		ptr = h;
+		while (ptr != NULL)
+		{
+			numbers++;
+
+			if (ptr->str != NULL)
+			{
+				printf("[%d] %s\n", ptr->len, ptr->str);
+			}
+			else
+			{
+				printf("[0] (nil)");
+			}
+			ptr = ptr->next;
+		}
+		return (numbers);
+	}
+	else
+	{
+		printf("Error");
+		exit (98);
+	}
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+this is  a directory that contains a project on singly linked list
+they are many files, each containing a specific tasks
+it also contains files like header file of used functions and structures
