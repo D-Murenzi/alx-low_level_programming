d3753fc118f9918e4ef8150140e14e9249e446d1
D-Murenzi <murenzidavid01@gmail.com>
2023-09-25 15:00:29 -0700

    betty correction onn 1-list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * listint_len-calculate number of elements in a linked list
+ * @h: pointer to the list
+ * Return: the number of elements
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *ptr;
