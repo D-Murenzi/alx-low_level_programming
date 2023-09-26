b75b1d178b1be2bb5a77017c61c016af8dd46ca2
D-Murenzi <murenzidavid01@gmail.com>
2023-09-26 12:08:51 -0700

    another free list function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+/* this function frees a linked list and sets a pointer to the pointer*/
+/* to the list to null */
+
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2- frees a whole linked list and sets head to null
+ * @head: a pointer to a pointer to the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while ((*head) != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+	free(head);
+}
