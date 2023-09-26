0c7c7e251fa50db4d1b977ac176276ce623faf6b
D-Murenzi <murenzidavid01@gmail.com>
2023-09-26 00:13:15 -0700

    another correction of print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -23,8 +23,34 @@ size_t print_listint(const listint_t *h)
 	while (ptr->next != NULL)
 	{
 		a++;
-		ptr = ptr->next;
 		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+
 	}
+	printf("%d\n",ptr->n);
+	a++;
 	return (a);
+}
+
+int main(void)
+{
+    listint_t *head;
+    listint_t *new;
+    listint_t hello = {8, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->n = 9;
+    new->next = head;
+    head = new;
+    n = print_listint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
 }
