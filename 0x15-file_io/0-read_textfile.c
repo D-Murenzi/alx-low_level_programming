30095f5d17c19f1e360e1cf3c1cb094f73913602
D-Murenzi <murenzidavid01@gmail.com>
2023-10-04 14:43:26 -0700

    file that read a text and print it to the standard output"

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+/* this function reads a text from a file and print it standard output*/
+
+#include "main.h"
+
+/**
+ * read_textfile-reads a specified characters in the file and print them
+ * @filename: the path of the file to read from
+ * @letters: the number of characters to read from the file
+ * Return: number of characters printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int a;
+	int b;
+	char *array;
+
+	array = malloc((letters + 1));
+	if (array == NULL)
+	{
+		return (0);
+	}
+
+	if (filename)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd < 0)
+		{
+			return (0);
+		}
+		a = read(fd, array, letters);
+		if (a < 0)
+		{
+			return (0);
+		}
+		b = write(STDOUT_FILENO, array, letters);
+		if (a != b)
+		{
+			return (0);
+		}
+		return (b);
+	}
+	return (0);
+}
