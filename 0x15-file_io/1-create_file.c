4e80835877d2383701e2b57a01fcd43cd880d727
D-Murenzi <murenzidavid01@gmail.com>
2023-10-05 15:05:35 -0700

    function that creates a file and write to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+/* this function creates a file and write a content to it */
+
+#include "main.h"
+
+/**
+ * create_file-function to create a file and write a content to it
+ * @filename: the file to create
+ * @text_content: the content to write in the file
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	if (filename)
+	{
+		int fd;
+
+		fd = open(filename, O_RDWR | O_CREATE, S_IRUSR);
+		if (fd < 0)
+		{
+			return (-1);
+		}
+		if (text_content)
+		{
+			trancate(filename, 0);
+			b = write(fd, text_content, strlen(text_content));
+			if (b <= 0)
+			{
+				close(fd);
+				return (-1);
+			}
+		}
+		else
+		{
+			trancate(filename, 0);
+		}
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
