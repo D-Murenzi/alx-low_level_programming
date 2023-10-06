ed9a2061cad9c1d1dad41128e8d662c7d8260c82
D-Murenzi <murenzidavid01@gmail.com>
2023-10-06 15:11:13 -0700

    program that copies content of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 	fda = open(argv[1], O_RDONLY);
 	if (fda < 0)
 	{
-		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buff);
 		exit(98);
 	}
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 		   S_IWGRP | S_IRGRP | S_IROTH);
 	if (fdb < 0)
 	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(buff);
 		exit(99);
 	}
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 	{
 		if (a < 0)
 		{
-			dprintf(STDERR_FILENO, "Can't read from file %s\n",
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 				argv[1]);
 			close(fda);
 			close(fdb);
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 		b = write(fdb, buff, 1024);
 		if (b < 0)
 		{
-			dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fda);
 			close(fdb);
 			free(buff);
@@ -73,14 +73,14 @@ int main(int argc, char *argv[])
 	f = close(fda);
 	if (f != 0)
 	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", fda);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fda);
 		free(buff);
 		exit(100);
 	}
 	g = close(fdb);
 	if (g != 0)
 	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", fdb);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdb);
 		free(buff);
 		exit(100);
 	}
