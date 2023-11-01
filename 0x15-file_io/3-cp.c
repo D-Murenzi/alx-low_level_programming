5f14507d68c53e8435a386a3baac00fec3809a69
D-Murenzi <murenzidavid01@gmail.com>
2023-11-01 22:36:46 +0200

    debugged copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ * c_open-opens a file for reading or writing
+ * @filename: the file to be openned
+ * @flags: int values specifying the permissions to open file with
+ * @mode: the permissions for oepned or created file
+ * Return: the file descriptor to the file
+ */
+
 int c_open(char *filename, int flags, int mode)
 {
 	int a;
-	
+
 	a = open(filename, flags, mode);
 	if (a < 0)
 	{
@@ -18,6 +26,15 @@ int c_open(char *filename, int flags, int mode)
 	}
 	return (a);
 }
+
+/**
+ * c_read-reads from the file descriptor
+ * @fd: descriptor to read from
+ * @ptr: buffer to read and put
+ * @bytes: the number of bytes to read
+ * @filename: the file to read from
+ * Return: the number of bytes read
+ */
 
 int c_read(int fd, char *ptr, int bytes, char *filename)
 {
@@ -34,7 +51,16 @@ int c_read(int fd, char *ptr, int bytes, char *filename)
 	return (a);
 }
 
-int c_write( int fd, char *ptr, int bytes, char *filename)
+/**
+ * c_write-function thay write bytes to file descriptor
+ * @fd: the descriptor to write to
+ * @ptr: the buffer to read from
+ * @bytes: the bytes to write
+ * @filename: the file to write to
+ * Return: the number of bytes read
+ */
+
+int c_write(int fd, char *ptr, int bytes, char *filename)
 {
 	int a;
 
@@ -49,6 +75,12 @@ int c_write( int fd, char *ptr, int bytes, char *filename)
 	return (a);
 }
 
+/**
+ * c_close-function that closes file descriptor
+ * @fd:the file descriptor to close
+ * Return: nothing
+ */
+
 void c_close(int fd)
 {
 	int a;
@@ -60,12 +92,18 @@ void c_close(int fd)
 		exit(100);
 	}
 }
-		
+
+/**
+ * main-entry to the program and calling other functions
+ * @argc: the number of arguments passed to prog
+ * @argv: the array to the arguments
+ * Return: 0 on success and any number on failure
+ */
+
 int main(int argc, char *argv[])
 {
-	int a, b;
-	char *ptr;
-	int c;
+	char *ptr, *ptr2;
+	int a, b, d = 1, c = -1;
 
 	if (argc != 3)
 	{
@@ -79,13 +117,19 @@ int main(int argc, char *argv[])
 	}
 	a = c_open(argv[1], O_RDONLY, 0);
 	b = c_open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-
-	c = -1;
-	while(c != 0)
+	ptr2 = ptr;
+	while (c != 0)
 	{
 		c = c_read(a, ptr, 1024, argv[1]);
-		c_write(b, ptr, c, argv[2]);
+		c_write(b, ptr, 1024, argv[2]);
+		while (d <= 1025)
+		{
+			*ptr2 = '\0';
+			ptr2++;
+			d++;
+		}
 	}
+	dprintf(b, "\n");
 	free(ptr);
 	if (close(a))
 	{
@@ -99,5 +143,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-		
-		
