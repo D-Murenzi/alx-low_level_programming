b33fbb408c3ecb8dda9db7fe91261f7c787b0ec9
D-Murenzi <murenzidavid01@gmail.com>
2023-12-07 22:30:01 +0200

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+/* this function returns the elements in a doubly linked list*/
+
+#include "lists.h"
+
+/**b
+ * dlistint_len-function that returns anumber of elements in the list
+ * @h: the pointer to a double linked list
+ * Return: number of elements in linked lists
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	size_t a;
+
+	ptr = h;
+	a = 0;
+	while (ptr != NULL)
+	{
+		if (ptr->n)
+		{
+			a++;
+		}
+		ptr = ptr->next;
+	}
+	return (a);
+}
