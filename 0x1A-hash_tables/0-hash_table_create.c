20190e8897b60886949689a7b0e0fc1f99752476
D-Murenzi <murenzidavid01@gmail.com>
2023-12-22 13:32:53 +0200

    debugged task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,19 +10,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ptr;
+	if (size)
+	{
+		hash_table_t *ptr;
 
-	ptr = (hash_table_t *)malloc(sizeof(hash_table_t));
-	if (ptr == NULL)
-	{
-		return (NULL);
+		ptr = (hash_table_t *)malloc(sizeof(hash_table_t));
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+
+		ptr->size = size;
+		ptr->array = malloc(sizeof(hash_table_t) * size);
+		if (ptr->array == NULL)
+		{
+			return (NULL);
+		}
+		return (ptr);
 	}
-	
-	ptr->size = size;
-	ptr->array = malloc(sizeof(hash_table_t) * size);
-	if (ptr->array == NULL)
-	{
-		return NULL;
-	}
-	return (ptr);
+	return (NULL);
 }
