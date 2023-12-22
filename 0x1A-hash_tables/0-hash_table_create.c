06e189905e25a789963ccba910f13024245d5d15
D-Murenzi <murenzidavid01@gmail.com>
2023-12-22 13:26:51 +0200

    new directory of hash tables, readme file, header file and task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+/* this file contains a function that creates a hash_table*/
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_create-creates a hash table
+ * @size: the size of hash table to be created
+ * Return: pointer to the hash_table created
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ptr;
+
+	ptr = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	
+	ptr->size = size;
+	ptr->array = malloc(sizeof(hash_table_t) * size);
+	if (ptr->array == NULL)
+	{
+		return NULL;
+	}
+	return (ptr);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+This directory is about hashtables in c.
+It is for learning purpose, for more contact the Author.
+Author: D-Murenzi.
